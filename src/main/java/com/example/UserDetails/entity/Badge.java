23937c48e40237dc1d14d7dae36b8264f55bf5fb
Shikhar Saxena <shikharsaxena@Shikhars-MacBook-Pro.local>
2021-03-12 12:03:44 +0530

    UserDetails Service commit

--- src/main/java/com/example/UserDetails/entity/Badge.java
+++ src/main/java/com/example/UserDetails/entity/Badge.java
@@ -9,7 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 
-@Entity(name = "bagde")
+@Entity(name = "badge")
 @Data
 public class Badge {
     @Id
